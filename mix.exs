9135bcce75b99dcae93953b043a0b9b4e8aa4517
James Harton <james@harton.dev>
2026-05-31 09:58:00 +0000

    docs: add beam-bots logo to hex docs

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule BB.Reactor.MixProject do
   defp docs do
     [
       main: "readme",
+      logo: "assets/logo.png",
       extras:
         ["README.md", "CHANGELOG.md"]
         |> Enum.concat(Path.wildcard("documentation/**/*.{md,livemd,cheatmd}")),
